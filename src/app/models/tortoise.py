491653d54cc9054e9ff09d869df3445ad11ca6ea
Derek Wan <d.wan@icloud.com>
2022-05-01 09:48:18 +0800

    Add explicit PK column

--- src/app/models/tortoise.py
+++ src/app/models/tortoise.py
@@ -1,9 +1,11 @@
 from tortoise.fields import DatetimeField
+from tortoise.fields import IntField
 from tortoise.fields import TextField
 from tortoise.models import Model
 
 
 class TextSummary(Model):
+    id = IntField(pk=True)
     url = TextField()
     summary = TextField()
     created_at = DatetimeField(auto_now_add=True)
