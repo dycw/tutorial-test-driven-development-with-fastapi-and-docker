baa1003281c6fb54d8569c41b517a1db83598c0b
Derek Wan <d.wan@icloud.com>
2022-05-01 08:23:25 +0800

    Finish "Pytest Setup" (#4)
    * Add tortoise ORM
    
    * Update
    
    * Add

---
+++ src/tests/conftest.py
@@ -0,0 +1,22 @@
+from collections.abc import Iterator
+from os import getenv
+
+from pytest import fixture
+from starlette.testclient import TestClient
+
+from app.config import Settings
+from app.config import get_settings
+from app.main import app
+
+
+def get_settings_override() -> Settings:
+    return Settings(
+        testing=True, database_url=getenv("DATABASE_TEST_URL")  # type: ignore
+    )
+
+
+@fixture(scope="module")
+def test_app() -> Iterator[TestClient]:
+    app.dependency_overrides[get_settings] = get_settings_override
+    with TestClient(app) as test_client:
+        yield test_client
