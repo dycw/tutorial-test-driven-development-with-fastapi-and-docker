d7b71b3cde92991dc99d963ebc5233b22dcdbd7f
Derek Wan <d.wan@icloud.com>
2023-04-11 16:36:45 +0900

    Reset back to `Getting Started`

--- src/app/config.py
+++ src/app/config.py
@@ -1,22 +1,20 @@
 from functools import lru_cache
 from logging import getLogger
-from os import getenv
+from typing import cast
 
 from beartype import beartype
 from pydantic import BaseSettings
-from pydantic.networks import AnyUrl
 
 _LOGGER = getLogger("uvicorn")
 
 
 class Settings(BaseSettings):
-    environment: str = getenv("ENVIRONMENT", "dev")
-    testing: bool = getenv("TESTING", 0)  # type: ignore
-    database_url: AnyUrl = getenv("DATABASE_URL")  # type: ignore
+    environment: str = "dev"
+    testing: bool = cast(bool, 0)
 
 
 @lru_cache
 @beartype
-def get_settings() -> BaseSettings:
+def get_settings() -> Settings:
     _LOGGER.info("Loading config settings from the environment...")
     return Settings()
