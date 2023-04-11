657fdd8f83e75cd1c30d84831c7acecf62e280a2
Derek Wan <d.wan@icloud.com>
2023-04-11 17:50:00 +0900

    Finish "Pytest setup"

---
+++ src/tests/test_ping.py
@@ -0,0 +1,9 @@
+from beartype import beartype
+from starlette.testclient import TestClient
+
+
+@beartype
+def test_ping(*, test_app: TestClient) -> None:
+    response = test_app.get("/ping")
+    assert response.status_code == 200
+    assert response.json() == {"environment": "dev", "ping": "pong!", "testing": True}
