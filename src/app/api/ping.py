9b12c78fe2f3cca991b52ee5cab0f08d97a66b3b
Derek Wan <d.wan@icloud.com>
2023-04-12 13:27:57 +0900

    Finish "App structure" (#20)

---
+++ src/app/api/ping.py
@@ -0,0 +1,16 @@
+from beartype import beartype
+from fastapi import APIRouter, Depends
+
+from app.config import Settings, get_settings
+
+router = APIRouter()
+
+
+@router.get("/ping")
+@beartype
+async def pong(*, settings: Settings = Depends(get_settings)) -> dict[str, str | bool]:
+    return {
+        "ping": "pong!",
+        "environment": settings.environment,
+        "testing": settings.testing,
+    }
--- src/app/db.py
+++ src/app/db.py
@@ -1,4 +1,13 @@
+from logging import getLogger
 from os import environ
+
+from beartype import beartype
+from fastapi import FastAPI
+from tortoise import Tortoise, run_async
+from tortoise.contrib.fastapi import register_tortoise
+
+_logger = getLogger("uvicorn")
+
 
 TORTOISE_ORM = {
     "connections": {"default": environ.get("DATABASE_URL")},
@@ -9,3 +18,28 @@ TORTOISE_ORM = {
         }
     },
 }
+
+
+@beartype
+def init_db(app: FastAPI, /) -> None:
+    register_tortoise(
+        app,
+        db_url=environ.get("DATABASE_URL"),
+        modules={"models": ["app.models.tortoise"]},
+        add_exception_handlers=True,
+    )
+
+
+@beartype
+async def generate_schema() -> None:
+    _logger.info("Initializing Tortoise...")
+    await Tortoise.init(
+        db_url=environ.get("DATABASE_URL"), modules={"models": ["models.tortoise"]}
+    )
+    _logger.info("Generating database schema via Tortoise...")
+    await Tortoise.generate_schemas()
+    await Tortoise.close_connections()
+
+
+if __name__ == "__main__":
+    run_async(generate_schema())
