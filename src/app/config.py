b9455b097013f0246f83b1124d30021e5702bf72
Derek Wan <d.wan@icloud.com>
2023-04-12 11:33:55 +0900

    Strip down to `Getting started`

--- src/app/config.py
+++ src/app/config.py
@@ -2,8 +2,7 @@ from functools import lru_cache
 from logging import getLogger
 from typing import cast
 
-from beartype import beartype
-from pydantic import AnyUrl, BaseSettings
+from pydantic import BaseSettings
 
 _LOGGER = getLogger("uvicorn")
 
@@ -11,11 +10,9 @@ _LOGGER = getLogger("uvicorn")
 class Settings(BaseSettings):
     environment: str = "dev"
     testing: bool = cast(bool, 0)
-    database_url: AnyUrl = cast(AnyUrl, None)
 
 
 @lru_cache
-@beartype
 def get_settings() -> Settings:
     _LOGGER.info("Loading config settings from the environment...")
     return Settings()
--- src/app/main.py
+++ src/app/main.py
@@ -1,24 +1,11 @@
-from os import environ
-
-from beartype import beartype
 from fastapi import Depends, FastAPI
-from tortoise.contrib.fastapi import register_tortoise
 
 from app.config import Settings, get_settings
 
 app = FastAPI()
 
 
-register_tortoise(
-    app,
-    db_url=environ.get("DATABASE_URL"),
-    modules={"models": ["app.models.tortoise"]},
-    add_exception_handlers=True,
-)
-
-
 @app.get("/ping")
-@beartype
 async def pong(*, settings: Settings = Depends(get_settings)) -> dict[str, str | bool]:
     return {
         "ping": "pong!",
