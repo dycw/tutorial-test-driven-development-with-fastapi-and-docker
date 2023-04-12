b41adaada931f1e93873219636833ff632918f4a
Derek Wan <d.wan@icloud.com>
2023-04-12 17:10:19 +0900

    Try CD, edit `ping`

--- src/app/api/ping.py
+++ src/app/api/ping.py
@@ -10,7 +10,7 @@ router = APIRouter()
 @beartype
 async def pong(*, settings: Settings = Depends(get_settings)) -> dict[str, str | bool]:
     return {
-        "ping": "pong",
+        "ping": "pong2",
         "environment": settings.environment,
         "testing": settings.testing,
     }
